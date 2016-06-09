b1fd1c58fd3439205f1e5fd4c64d3ee4c411db13
Czerniecki Wojciech <czerniecki.wojciech@gmail.com>
2016-06-09 18:28:01 +0200

    cdn2

---
+++ Assets/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject GameOverUI;
+
+    private bool lose = false;
+    static bool gameover_bool = true;
+
+    void Start()
+    {
+        //show(true);
+    }
+
+    void Update() {
+        Debug.logger.Log(gameover_bool);
+        //show(gameover_bool);
+    }
+
+    public void show(bool state) {
+        GameOverUI.SetActive(state);
+    }
+}
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject PauseUI;
+    public GameObject GameOverUI;
+
+    private bool paused = false;
+
+    void Start() {
+        PauseUI.SetActive(false);
+        GameOverUI.SetActive(false);
+    }
+
+    bool isPlayerOnScene()
+    {
+        return GameObject.Find("Player");
+    }
+
+    void Update() {
+        Debug.logger.Log("Dupa");
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        if(!isPlayerOnScene()) {
+            GameOverUI.SetActive(true);
+        }
+    }
+
+
+    public void Resume() {
+        paused = false;
+
+    }
+
+    public void Restart(){
+        int buildIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
